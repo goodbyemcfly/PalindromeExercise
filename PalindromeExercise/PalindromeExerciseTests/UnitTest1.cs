cb2b8ab3ded7ecb1e8721c4d31f247cc8347128b
Brett Bellomy <brettbellomymusic@gmail.com>
2024-05-23 13:27:31 -0500

    first commit: completed assingment

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("wow", true)]
+        [InlineData("cowboy", false)]
+        [InlineData("mom", true)]
 
+        public void IsAPalindromeTest(string word, bool expected)
+        {
+            var wordSmith = new WordSmith();
+
+            var actual = wordSmith.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
